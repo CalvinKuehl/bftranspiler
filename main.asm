eb0062e902ba142231ec6f8c1f78136476a89229
Calvin Kuehl <21591800-CalvinKuehl@users.noreply.replit.com>
2025-04-02 12:24:17 +0000

    Finished the c code (optimizations are next)

--- main.asm
+++ main.asm
@@ -1,11 +1,29 @@
 section .text
 global _start
 _start:
-	cmp byte [rsp], 0
-	jne ej1 ;jump to end jump 1 if the pointer is zero
-	bj1: ; begin jump 1
-	ej1: ; end jump 1
-	cmp byte [rsp], 0
-	jnz bj1 ;jump to begin jump 1 if the pointer is nonzero
+	inc rsp
+	mov rsi, rsp
+	mov rdx, 1
+	mov rdi, 0
+	mov rax, 0x0
+	syscall
+	inc rsp
+	mov rsi, rsp
+	mov rdx, 1
+	mov rdi, 0
+	mov rax, 0x0
+	syscall
+	dec rsp
+	mov rsi, rsp
+	mov rdx, 1
+	mov rdi, 0
+	mov rax, 0x1
+	syscall
+	inc rsp
+	mov rsi, rsp
+	mov rdx, 1
+	mov rdi, 0
+	mov rax, 0x1
+	syscall
 	mov rax, 0x3c
 	syscall
--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(int argc, char* argv[]) {
 	FILE *outfile;
 	FILE *infile;
 	char givenchar;
+	int jmpnum = 0;
 	// reset write file
 	outfile = fopen(argv[1], "w");
 	fclose(outfile);
@@ -14,10 +15,10 @@ int main(int argc, char* argv[]) {
 	// open read file (brainfuck)
 	infile = fopen(argv[2], "r");
 	// get starter code for the assembly (defining main function, etc)
-	fprintf(outfile, "section .text\nglobal _start\n_start:");
+	fprintf(outfile, "section .text\nglobal _start\n_start:\n\tinc rsp");
 	// get the rest of the code (what is actually from the brainfuck coode)
 	givenchar = fgetc(infile);
-	while ( givenchar != EOF) {1
+	while ( givenchar != EOF) {
 		switch (givenchar) {
 			case '+': // increment byte
 				fprintf(outfile, "\n\tinc byte [rsp]");
@@ -28,11 +29,21 @@ int main(int argc, char* argv[]) {
 			case '.': // output byte
 				fprintf(outfile, "\n\tmov rsi, rsp\n\tmov rdx, 1\n\tmov rdi, 0\n\tmov rax, 0x1\n\tsyscall");
 				break;
+			case ',': // input byte
+				fprintf(outfile, "\n\tmov rsi, rsp\n\tmov rdx, 1\n\tmov rdi, 0\n\tmov rax, 0x0\n\tsyscall");
+				break;
 			case '>': // increment pointer
 				fprintf(outfile, "\n\tinc rsp");
 				break;
 			case '<': // decrement pointer
 				fprintf(outfile, "\n\tdec rsp");
+				break;
+			case '[': // begin loop
+				jmpnum++;
+				fprintf(outfile, "\n\tcmp byte [rsp], 0\n\tje ej%i\n\tjmp bj%i\n\tbj%i:", jmpnum, jmpnum, jmpnum);
+				break;
+			case ']':
+				fprintf(outfile, "\n\tcmp byte [rsp], 0\n\tjnz bj%i\n\tej%i:", jmpnum, jmpnum);
 				break;
 			default:
 				break;
